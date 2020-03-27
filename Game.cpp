4b2e3ae4f20376a28cbb71fc30e78107c99de0f6
Edouard LE BEUZE <edouard.lebeuze@gmail.com>
2020-03-27 18:26:35 +0100

    view and background

--- Game.cpp
+++ Game.cpp
@@ -3,9 +3,12 @@
 Game::Game(QWidget *parent): QGraphicsView(parent){
     // create a scene
     scene = new QGraphicsScene();
-    scene->setSceneRect(0, 0, 800, 600);    //the scene's size
+    scene->setSceneRect(0, 0, 10000, 1080);    // the scene's size
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    setFixedSize(800, 600);     // the view's size
     setScene(scene);
+
+    QImage background(":/images/images/Background.png");     // l'image de background
+    QImage img2 = background.scaled(1920, 1080, Qt::KeepAspectRatio);      // redimension du background
+    setBackgroundBrush(img2);       // affichage
 }
